5df1eb7ec31ef6f0229626188ce08484add9105a
Deesjes <85071119+Deesjes@users.noreply.github.com>
2021-07-11 19:48:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let myAge = 34;
+
+if (myAge => 18) {
+
+    console.log("You are allowed to enter");
+
+} else {
+    console.log("you are to young, please come back when you have turned 18");
+
+}
+
+
+let isFemale = true;
+
+if (isFemale = "true") {
+
+    console.log("You are female")
+} else {
+
+    console.log("you are male")
+}
+
+let driverStatus = bob;
+
+if (driverStatus = "bob") {
+    console.log("you are allowed to drive.");
+
+} else {
+    console.log("you are drunk, call a cab.");
+
+}
